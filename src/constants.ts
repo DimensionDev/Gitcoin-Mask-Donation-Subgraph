b82b03c49f851b81d9561327696b49043a2107d3
guanbinrui <guanbinrui@dimension.im>
2021-01-29 09:45:55 +0800

    :sparkles: support handleSplitTransfer

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 export const CHAIN_ID = 1;
 
+export const MASK_ADDRESS = "0x7cbba07e31dc7b12bb69a1209c5b11a8ac50acf5"
 export const GITCOIN_MAINTAINER_ADDRESS = "0x00De4B13153673BCAE2616b67bf822500d325Fc3";
 export const GITCOIN_ETHER_ADDRESS = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
 
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,15 +1,51 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Address, BigInt, log } from "@graphprotocol/graph-ts"
 import { Splitter, SplitTransferCall } from "../generated/Splitter/Splitter"
 import { DonateCall, DonationSent } from "../generated/BulkCheckout/BulkCheckout"
+import { fetchToken } from "./helpers"
+import { MASK_ADDRESS } from "./constants"
+import { Donation, Donor } from "../generated/schema"
 
-function handleSplitTransfer(call: SplitTransferCall): void {
+export function handleSplitTransfer(call: SplitTransferCall): void {
+    let address = Address.fromHexString(MASK_ADDRESS) as Address
 
+    // not a Mask related donation
+    if (call.inputs.toFirst != address && call.inputs.toSecond != address) {
+        return
+    }
+
+    // create token
+    let token = fetchToken(call.inputs.tokenAddress)
+    token.save()
+
+    // create donor
+    let donor = new Donor(call.from.toHexString())
+    donor.address = call.from
+    donor.save()
+
+    // create donation 
+    let donation = new Donation(call.transaction.hash.toHexString())
+    donation.tx_id = call.transaction.hash.toHexString()
+    donation.token = token.id
+    donation.donor = donor.id
+    donation.dest = address
+    donation.creation_time = call.block.timestamp.toI32()
+
+    // the first one is the donation for Mask
+    if (call.inputs.toFirst == address) {
+        donation.total = call.inputs.valueFirst
+    }
+
+    // the second one is the donation for Mask
+    if (call.inputs.toSecond == address) {
+        donation.total = call.inputs.valueSecond
+    }
+    donation.save()
 }
 
-function handleDonate(call: DonateCall): void {
-
+export function handleDonate(call: DonateCall): void {
+    log.info('handle donate', [])
 }
 
-function handleDonationSent(event: DonationSent): void {
-    
+export function handleDonationSent(event: DonationSent): void {
+    log.info('handle donation sent event', [])
 }
